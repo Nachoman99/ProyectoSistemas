25b44fdfe63617232c29ea0dcef13a5c1041b10e
Kevin Trejos <trejoscuberok@gmail.com>
2019-11-09 10:30:28 -0600

    Prueba GIT

--- SistemasProyecto/SistemasProyecto/Program.vb
+++ SistemasProyecto/SistemasProyecto/Program.vb
@@ -4,5 +4,6 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
         Console.WriteLine("Hola Soy Nacho")
+        Console.WriteLine("Hola Putos")
     End Sub
 End Module
